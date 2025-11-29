280cd244d17fa0023115a896440bcd72ccfe6cf6
stebeus <stebeus@mailfence.com>
2025-11-29 11:24:17 -0300

    Create Book constructor

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 "use strict";
 
 const myLibrary = [];
+
+function Book(title, author, pageQuantity, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.pageQuantity = pageQuantity;
+  this.readStatus = readStatus;
+  this.uuid = crypto.randomUUID();
+}
